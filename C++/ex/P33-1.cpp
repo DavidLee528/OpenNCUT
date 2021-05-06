bfb0920aa002adb1e354de79ab340111a090c6a3
DavidLee528 <35065046+DavidLee528@users.noreply.github.com>
2021-05-06 12:29:06 +0800

    Add files via upload

---
+++ C++/ex/P33-1.cpp
@@ -0,0 +1,17 @@
+/*
+ * @ Author: 李天昊
+ * @ Description: 
+ * @ Date: 20210306
+ * @ E-mail: 13121515269@163.com
+ */
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    cout << "This" << "is"; 
+    cout << "a" << "C++"; 
+    cout << "program." << endl; 
+    return 0; 
+}
